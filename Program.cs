22fb918c0b6b687571c9031b5e0534b419ec8e1a
Васильева Дарья <dasavasileva233@gmail.com>
2026-09-25 11:51:22 +0300

    feat: vs net logic

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+
+var data = new
+{
+    Student = "Васильева Дарья",
+    Group = "УИР-1",
+    Lab = "Лабораторная работа №1",
+    Technology = "Visual Studio 2022 + C# + Newtonsoft.Json"
+};
+
+string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+Console.WriteLine("Данные в формате JSON:");
+Console.WriteLine(json);
+Console.WriteLine();
+Console.WriteLine("Лабораторная работа №1 выполнена!");
